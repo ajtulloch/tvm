b1b323e1baf68c01d3e666f5fda33d8adc2153d0
Andrew Tulloch <andrew@tullo.ch>
2018-05-25 12:30:02 -0700

    Application.mk builds with libstdc++
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:

--- apps/android_rpc/app/src/main/jni/Application.mk
+++ apps/android_rpc/app/src/main/jni/Application.mk
@@ -1,16 +1,17 @@
 ifndef config
 	ifneq ("$(wildcard ./config.mk)","")
-	  config ?= config.mk
+		config ?= config.mk
 	else
-	  config ?= make/config.mk
+		config ?= make/config.mk
 	endif
 endif
 
 include $(config)
 
-APP_STL := gnustl_static
+APP_STL:=c++_static
 
-APP_CPPFLAGS += -DDMLC_LOG_STACK_TRACE=0 -DTVM4J_ANDROID=1 -std=c++11 -Oz -frtti
-ifeq ($(USE_OPENCL), 1)                                                                                                                                             
+
+APP_CPPFLAGS += -DDMLC_LOG_STACK_TRACE=0 -DTVM4J_ANDROID=1 -std=c++11 -Os -frtti
+ifeq ($(USE_OPENCL), 1)
 	APP_CPPFLAGS += -DTVM_OPENCL_RUNTIME=1
 endif

171a305d06bd5f5936d70829557c0f8403468bfb
Andrew Tulloch <andrew@tullo.ch>
2018-07-26 15:29:31 -0700

    Mobile

--- tensorize/gemm__neon.c
+++ tensorize/gemm__neon.c
@@ -1,35 +1,8 @@
+#include <arm_neon.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <arm_neon.h>
-
-static inline float32x4_t vld1q_f32_aligned(const float *address) {
-  return vld1q_f32(
-      (const float *)__builtin_assume_aligned(address, sizeof(float32x4_t)));
-}
-
-static inline void vst1q_f32_aligned(float* address, float32x4_t vector) {
-  vst1q_f32((float*) __builtin_assume_aligned(address, sizeof(float32x4_t)), vector);
-}
-
-static inline float32x4_t vmuladdq_lane0_f32(float32x4_t acc, float32x4_t a,
-                                             float32x2_t b) {
-#if defined(__aarch64__)
-  return vfmaq_lane_f32(acc, a, b, 0);
-#else
-  return vmlaq_lane_f32(acc, a, b, 0);
-#endif
-}
-
-static inline float32x4_t vmuladdq_lane1_f32(float32x4_t acc, float32x4_t a,
-                                             float32x2_t b) {
-#if defined(__aarch64__)
-  return vfmaq_lane_f32(acc, a, b, 1);
-#else
-  return vmlaq_lane_f32(acc, a, b, 1);
-#endif
-}
 
 void sgemm_compute_6x8__neon(int32_t k, const float *a, int32_t a_off,
                              const float *b, int32_t b_off, float *c,
@@ -78,8 +51,7 @@ void sgemm_compute_6x8__neon(int32_t k, const float *a, int32_t a_off,
                : [c] "+r"(c), [b] "+r"(b), [a] "+r"(a),
                  [k_size_t] "+r"(k_size_t), [ldc_size_t] "+r"(ldc_size_t)
                :
-               : "cc",
-                 "memory",
+               : "cc", "memory",
                  // note: someone on internet says that quad registers are
                  // unsupported in the clobber list!
                  "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9",
@@ -87,62 +59,6 @@ void sgemm_compute_6x8__neon(int32_t k, const float *a, int32_t a_off,
                  "d19", "d20", "d21", "d22", "d23", "d24", "d25", "d26", "d27",
                  "d28", "d29", "d30", "d31");
 }
-
-/* void sgemm_compute_6x8__neon(int32_t k, const float *a, int32_t a_off, */
-/*                             const float *b, int32_t b_off, float *c, */
-/*                             int32_t c_off, int32_t ldc) { */
-/*   a = a + a_off; */
-/*   b = b + b_off; */
-/*   c = c + c_off; */
-
-/*   float32x4_t vc00 = vdupq_n_f32(0.0f), vc01 = vdupq_n_f32(0.0f); */
-/*   float32x4_t vc10 = vdupq_n_f32(0.0f), vc11 = vdupq_n_f32(0.0f); */
-/*   float32x4_t vc20 = vdupq_n_f32(0.0f), vc21 = vdupq_n_f32(0.0f); */
-/*   float32x4_t vc30 = vdupq_n_f32(0.0f), vc31 = vdupq_n_f32(0.0f); */
-/*   float32x4_t vc40 = vdupq_n_f32(0.0f), vc41 = vdupq_n_f32(0.0f); */
-/*   float32x4_t vc50 = vdupq_n_f32(0.0f), vc51 = vdupq_n_f32(0.0f); */
-/*   do { */
-/*     const float32x4_t va0123 = vld1q_f32(a); */
-/*     const float32x2_t va45 = vld1_f32(a + 4); */
-/*     a += 6; */
-
-/*     const float32x4_t vb0 = vld1q_f32_aligned(b + 0); */
-/*     const float32x4_t vb1 = vld1q_f32_aligned(b + 4); */
-/*     b += 8; */
-
-/*     vc00 = vmuladdq_lane0_f32(vc00, vb0, vget_low_f32(va0123)); */
-/*     vc10 = vmuladdq_lane1_f32(vc10, vb0, vget_low_f32(va0123)); */
-/*     vc20 = vmuladdq_lane0_f32(vc20, vb0, vget_high_f32(va0123)); */
-/*     vc30 = vmuladdq_lane1_f32(vc30, vb0, vget_high_f32(va0123)); */
-/*     vc40 = vmuladdq_lane0_f32(vc40, vb0, va45); */
-/*     vc50 = vmuladdq_lane1_f32(vc50, vb0, va45); */
-
-/*     vc01 = vmuladdq_lane0_f32(vc01, vb1, vget_low_f32(va0123)); */
-/*     vc11 = vmuladdq_lane1_f32(vc11, vb1, vget_low_f32(va0123)); */
-/*     vc21 = vmuladdq_lane0_f32(vc21, vb1, vget_high_f32(va0123)); */
-/*     vc31 = vmuladdq_lane1_f32(vc31, vb1, vget_high_f32(va0123)); */
-/*     vc41 = vmuladdq_lane0_f32(vc41, vb1, va45); */
-/*     vc51 = vmuladdq_lane1_f32(vc51, vb1, va45); */
-/*   } while (--k); */
-
-/*   vst1q_f32(c + 0, vc00); */
-/*   vst1q_f32(c + 4, vc01); */
-/*   c += ldc; */
-/*   vst1q_f32(c + 0, vc10); */
-/*   vst1q_f32(c + 4, vc11); */
-/*   c += ldc; */
-/*   vst1q_f32(c + 0, vc20); */
-/*   vst1q_f32(c + 4, vc21); */
-/*   c += ldc; */
-/*   vst1q_f32(c + 0, vc30); */
-/*   vst1q_f32(c + 4, vc31); */
-/*   c += ldc; */
-/*   vst1q_f32(c + 0, vc40); */
-/*   vst1q_f32(c + 4, vc41); */
-/*   c += ldc; */
-/*   vst1q_f32(c + 0, vc50); */
-/*   vst1q_f32(c + 4, vc51); */
-/* } */
 
 void sgemm_reset_6x8__neon(float *c, int32_t c_off, int32_t ldc) {
   c = c + c_off;
@@ -231,8 +147,7 @@ void sgemm_update_6x8__neon(int32_t k, const float *a, int32_t a_off,
                : [c] "+r"(c), [b] "+r"(b), [a] "+r"(a),
                  [k_size_t] "+r"(k_size_t), [ldc_size_t] "+r"(ldc_size_t)
                :
-               : "cc",
-                 "memory",
+               : "cc", "memory",
                  // note: someone on internet says that quad registers are
                  // unsupported in the clobber list!
                  "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9",

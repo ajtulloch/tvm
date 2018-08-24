4cebfb323b72dfc7d0f4d5573ea28d87dd99ad24
Andrew Tulloch <andrew@tullo.ch>
2018-08-23 20:49:10 -0700

    Int8 -> Int32 GEMM
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:

--- tensorize/gemm__avx2.c
+++ tensorize/gemm__avx2.c
@@ -420,3 +420,112 @@ void sgemm_update_4x24__avx2(int32_t k, const float *a, int32_t a_off,
                  "%ymm5", "%ymm6", "%ymm7", "%ymm8", "%ymm9", "%ymm10",
                  "%ymm11", "%ymm12", "%ymm13", "%ymm14", "%ymm15");
 }
+
+void qgemm_compute_8x8x4__avx2(int32_t K, const uint8_t *A, int32_t a_off,
+                               const int8_t *B, int32_t b_off, int32_t *C,
+                               int32_t c_off, int32_t ldc) {
+  A += a_off;
+  B += b_off;
+  C += c_off;
+
+  uint64_t ldc_ = ldc * sizeof(C[0]);
+  uint64_t k_updated = 8 * K;
+  int16_t vone = 1;
+  int16_t *vonep = &vone;
+  asm volatile(
+#if !defined(__clang__)
+      "\t mov r8, %[k_updated]\n"
+      "\t mov r9, %[A]\n"
+      "\t mov rax, %[vonep]\n"
+      "\t mov r10, %[B]\n"
+      "\t mov rsi, %[B]\n"
+      "\t mov r12, %[C]\n"
+      "\t mov r13, %[ldc]\n"
+#else
+      "\t mov %[k_updated], %%r8\n"
+      "\t mov %[A], %%r9\n"
+      "\t mov %[vonep], %%rax\n"
+      "\t mov %[B], %%r10\n"
+      "\t mov %[B], %%rsi\n"
+      "\t mov %[C], %%r12\n"
+      "\t mov %[ldc], %%r13\n"
+      "\t .intel_syntax noprefix\n"
+#endif
+
+      // initialize all Ctile registers to zeros
+      "\t vzeroall\n"
+
+      // load constant "1"
+      "\t vpbroadcastw ymm15, WORD PTR [rax]\n"
+
+      "\t mov r14, 0\n"
+      "\t loop_inner%=:\n"
+
+      // <Main micro-kernel>
+      // load all B's
+      "\t mov rbx, r10\n"
+      "\t vmovaps ymm9,YMMWORD PTR [rbx]\n"
+      "\t vpbroadcastd ymm8,DWORD PTR [r9+r14 + 0]\n"
+      "\t vpmaddubsw ymm14, ymm8, ymm9\n"
+      "\t vpmaddwd ymm14, ymm15, ymm14\n"
+      "\t vpaddd ymm0, ymm0, ymm14\n"
+      "\t vpbroadcastd ymm10,DWORD PTR [r9+r14 + 4]\n"
+      "\t vpmaddubsw ymm14, ymm10, ymm9\n"
+      "\t vpmaddwd ymm14, ymm15, ymm14\n"
+      "\t vpaddd ymm1, ymm1, ymm14\n"
+      "\t vpbroadcastd ymm11,DWORD PTR [r9+r14 + 8]\n"
+      "\t vpmaddubsw ymm14, ymm11, ymm9\n"
+      "\t vpmaddwd ymm14, ymm15, ymm14\n"
+      "\t vpaddd ymm2, ymm2, ymm14\n"
+      "\t vpbroadcastd ymm12,DWORD PTR [r9+r14 + 12]\n"
+      "\t vpmaddubsw ymm14, ymm12, ymm9\n"
+      "\t vpmaddwd ymm14, ymm15, ymm14\n"
+      "\t vpaddd ymm3, ymm3, ymm14\n"
+      "\t vpbroadcastd ymm13,DWORD PTR [r9+r14 + 16]\n"
+      "\t vpmaddubsw ymm14, ymm13, ymm9\n"
+      "\t vpmaddwd ymm14, ymm15, ymm14\n"
+      "\t vpaddd ymm4, ymm4, ymm14\n"
+      "\t vpbroadcastd ymm8,DWORD PTR [r9+r14 + 20]\n"
+      "\t vpmaddubsw ymm14, ymm8, ymm9\n"
+      "\t vpmaddwd ymm14, ymm15, ymm14\n"
+      "\t vpaddd ymm5, ymm5, ymm14\n"
+      "\t vpbroadcastd ymm10,DWORD PTR [r9+r14 + 24]\n"
+      "\t vpmaddubsw ymm14, ymm10, ymm9\n"
+      "\t vpmaddwd ymm14, ymm15, ymm14\n"
+      "\t vpaddd ymm6, ymm6, ymm14\n"
+      "\t vpbroadcastd ymm11,DWORD PTR [r9+r14 + 28]\n"
+      "\t vpmaddubsw ymm14, ymm11, ymm9\n"
+      "\t vpmaddwd ymm14, ymm15, ymm14\n"
+      "\t vpaddd ymm7, ymm7, ymm14\n"
+
+      // loop increment
+      "\t add r10, 32\n"
+      "\t add r14, 4 * 8\n"
+      "\t cmp r14, r8\n"
+      "\t jl loop_inner%=\n"
+
+      "\t vmovups YMMWORD PTR [r12 + 0], ymm0\n"
+      "\t add r12, r13\n"
+      "\t vmovups YMMWORD PTR [r12 + 0], ymm1\n"
+      "\t add r12, r13\n"
+      "\t vmovups YMMWORD PTR [r12 + 0], ymm2\n"
+      "\t add r12, r13\n"
+      "\t vmovups YMMWORD PTR [r12 + 0], ymm3\n"
+      "\t add r12, r13\n"
+      "\t vmovups YMMWORD PTR [r12 + 0], ymm4\n"
+      "\t add r12, r13\n"
+      "\t vmovups YMMWORD PTR [r12 + 0], ymm5\n"
+      "\t add r12, r13\n"
+      "\t vmovups YMMWORD PTR [r12 + 0], ymm6\n"
+      "\t add r12, r13\n"
+      "\t vmovups YMMWORD PTR [r12 + 0], ymm7\n"
+
+      // register list
+      :
+      : [k_updated] "rm"(k_updated), [A] "rm"(A), [B] "rm"(B), [C] "rm"(C),
+        [vonep] "rm"(vonep), [ldc] "rm"(ldc_)
+      : "cc", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "rax",
+        "rbx", "rsi", "memory", "%ymm0", "%ymm1", "%ymm2", "%ymm3", "%ymm4",
+        "%ymm5", "%ymm6", "%ymm7", "%ymm8", "%ymm9", "%ymm10", "%ymm11",
+        "%ymm12", "%ymm13", "%ymm14", "%ymm15");
+}
--- tutorials/optimize/opt_gemm.py
+++ tutorials/optimize/opt_gemm.py
@@ -38,50 +38,121 @@ Intel i7-4770HQ CPU. The cache line size should be 64 bytes for all the x86 CPUs
 # Then we write a baseline implementation, the simplest way to write a matrix multiplication in TVM.
 
 import tvm
+from topi.util import get_const_tuple
 import numpy
 import timeit
+
+target = tvm.target.create("llvm -mcpu=core-avx2")
+ARCH = "avx2"
+
+BITCODE_PATHS = [
+    "tensorize/gemm__{ARCH}.bc".format(ARCH=ARCH)
+]
+
+@tvm.register_func("tvm_callback_llvm_bitcode_path")
+def bitcode_paths():
+    global BITCODE_PATHS
+    return BITCODE_PATHS
+
+# Tensorized
+def intrin_tile_gemm(KBlock, MTile, NTile, KTile):
+    # assert (M, N) in MNTiles, (M, N)
+    A = tvm.placeholder((KBlock, MTile, KTile), dtype='uint8', name='ATile')
+    B = tvm.placeholder((KBlock, NTile, KTile), dtype='int8', name='BTile')
+    kb = tvm.reduce_axis((0, KBlock), name='kblock')
+    kt = tvm.reduce_axis((0, KTile), name='ktile')
+    C = tvm.compute(
+        (MTile, NTile),
+        lambda m, n: tvm.sum(
+            A[kb, m, kt].astype('int32') * B[kb, n, kt].astype('int32'),
+            axis=[kb, kt]),
+        name='C')
+
+    Ab = tvm.decl_buffer(A.shape, A.dtype,
+                        name="A",
+                        offset_factor=KTile,
+                        strides=[MTile * KTile, KTile, 1])
+    Bb = tvm.decl_buffer(B.shape, B.dtype,
+                        name="B",
+                        offset_factor=KTile,
+                        strides=[NTile * KTile, KTile, 1])
+    Cb = tvm.decl_buffer(C.shape, C.dtype,
+                        name="C",
+                        offset_factor=1,
+                        strides=[tvm.var("ldc"), 1])
+
+    def intrin_func(ins, outs):
+        aa, bb = ins
+        print(outs[0])
+        print(outs[0].strides[0])
+        cc = outs[0]
+
+        def body():
+            irb = tvm.ir_builder.create()
+            extern_call = tvm.call_extern(
+                "int32",
+                "qgemm_compute_{M}x{N}x{K}__{ARCH}".format(M=MTile, N=NTile, K=KTile, ARCH=ARCH),
+                KBlock * KTile,
+                irb.buffer_ptr(aa),
+                aa.elem_offset,
+                irb.buffer_ptr(bb),
+                bb.elem_offset,
+                irb.buffer_ptr(cc),
+                cc.elem_offset,
+                cc.strides[0])
+            irb.emit(extern_call)
+            return irb.get()
+        return body()
+
+    with tvm.build_config():
+        return tvm.decl_tensor_intrin(C.op,
+                                      intrin_func,
+                                      binds={A: Ab, B: Bb, C: Cb})
 
 # The size of the matrix
 # (M, K) x (K, N)
 # You are free to try out different shapes, sometimes TVM optimization outperforms numpy with MKL.
-M = 1024
-K = 1024
-N = 1024
-
+M = 64
+K = 64
+N = 64
+FLOPS = 2 * M * N * K
+REPEAT = 10
 # The default tensor type in tvm
-dtype = "float32"
+dtype_input = "uint8"
+dtype_output = "uint32"
 
 # using Intel AVX2(Advanced Vector Extensions) ISA for SIMD
 # To get the best performance, please change the following line
 # to llvm -mcpu=core-avx2, or specific type of CPU you use
-target = 'llvm'
-ctx = tvm.context(target, 0)
+# target = 'llvm'
+ctx = tvm.context(str(target), 0)
 
 # Random generated tensor for testing
-a = tvm.nd.array(numpy.random.rand(M, K).astype(dtype), ctx)
-b = tvm.nd.array(numpy.random.rand(K, N).astype(dtype), ctx)
+a = tvm.nd.array(numpy.random.rand(M, K).astype(dtype_input), ctx)
+b = tvm.nd.array(numpy.random.rand(K, N).astype(dtype_input), ctx)
 
-np_repeat = 100
+
+np_repeat = REPEAT
 np_runing_time = timeit.timeit(setup='import numpy\n'
                                      'M = ' + str(M) + '\n'
                                      'K = ' + str(K) + '\n'
                                      'N = ' + str(N) + '\n'
-                                     'dtype = "float32"\n'
+                                     'dtype = "uint32"\n'
                                      'a = numpy.random.rand(M, K).astype(dtype)\n'
                                      'b = numpy.random.rand(K, N).astype(dtype)\n',
                                stmt='answer = numpy.dot(a, b)',
                                number=np_repeat)
-print("Numpy running time: %f" % (np_runing_time / np_repeat))
+print("Numpy running time: %f" % (FLOPS * np_repeat / np_runing_time / 1.0E9))
 
 answer = numpy.dot(a.asnumpy(), b.asnumpy())
 
 # Algorithm
 k = tvm.reduce_axis((0, K), 'k')
-A = tvm.placeholder((M, K), name='A')
-B = tvm.placeholder((K, N), name='B')
+A = tvm.placeholder((M, K), name='A', dtype=dtype_input)
+B = tvm.placeholder((K, N), name='B', dtype=dtype_input)
 C = tvm.compute(
            (M, N),
-           lambda x, y: tvm.sum(A[x, k] * B[k, y], axis=k),
+           lambda x, y: tvm.sum(A[x, k].astype(dtype_output) * B[k, y].astype(dtype_output), axis=k),
            name='C')
 
 # Default schedule
@@ -89,12 +160,13 @@ s = tvm.create_schedule(C.op)
 func = tvm.build(s, [A, B, C], target=target, name='mmult')
 assert func
 
-c = tvm.nd.array(numpy.zeros((M, N), dtype=dtype), ctx)
+c = tvm.nd.array(numpy.zeros((M, N), dtype=dtype_output), ctx)
 func(a, b, c)
 numpy.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
 
-evaluator = func.time_evaluator(func.entry_name, ctx, number=1)
-print('Baseline: %f' % evaluator(a, b, c).mean)
+evaluator = func.time_evaluator(func.entry_name, ctx, number=REPEAT)
+print('Baseline flops: %f' % (FLOPS / evaluator(a, b, c).mean / 1E9))
+
 
 ################################################################################################
 # In TVM, we can always inspect lower level IR to debug or optimize our schedule.
@@ -124,14 +196,14 @@ s[C].reorder(xo, yo, ko, ki, xi, yi)
 func = tvm.build(s, [A, B, C], target=target, name='mmult')
 assert func
 
-c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype), ctx)
+c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype_output), ctx)
 func(a, b, c)
 numpy.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
 
 # By simply tiling the loop 32x32, and hoisting ko, ki outside the blocking loops,
 # we can see big speedup compared with the baseline.
-evaluator = func.time_evaluator(func.entry_name, ctx, number=10)
-print('Opt1: %f' % evaluator(a, b, c).mean)
+evaluator = func.time_evaluator(func.entry_name, ctx, number=REPEAT)
+print('Opt1: %f' % (FLOPS / evaluator(a, b, c).mean / 1E9))
 
 ################################################################################################
 # Here is the generated IR after blocking.
@@ -160,12 +232,13 @@ s[C].vectorize(yi)
 func = tvm.build(s, [A, B, C], target=target, name='mmult')
 assert func
 
-c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype), ctx)
+c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype_output), ctx)
 func(a, b, c)
 numpy.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
 
-evaluator = func.time_evaluator(func.entry_name, ctx, number=10)
-print('Opt2: %f' % evaluator(a, b, c).mean)
+evaluator = func.time_evaluator(func.entry_name, ctx, number=REPEAT)
+print('Opt2: %f' % (FLOPS / evaluator(a, b, c).mean / 1E9))
+
 
 ################################################################################################
 # Here is the generated IR after vectorization.
@@ -193,12 +266,13 @@ s[C].vectorize(yi)
 func = tvm.build(s, [A, B, C], target=target, name='mmult')
 assert func
 
-c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype), ctx)
+c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype_output), ctx)
 func(a, b, c)
 numpy.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
 
-evaluator = func.time_evaluator(func.entry_name, ctx, number=10)
-print('Opt3: %f' % evaluator(a, b, c).mean)
+evaluator = func.time_evaluator(func.entry_name, ctx, number=REPEAT)
+print('Opt3: %f' % (FLOPS / evaluator(a, b, c).mean / 1E9))
+
 
 ################################################################################################
 # Here is the generated IR after loop permutation.
@@ -229,7 +303,7 @@ print(tvm.lower(s, [A, B, C], simple_mode=True))
 # We have to re-write the algorithm slightly.
 packedB = tvm.compute((N / bn, K, bn), lambda x, y, z: B[y, x * bn + z], name='packedB')
 C = tvm.compute((M, N),
-                lambda x, y: tvm.sum(A[x, k] * packedB[y / bn, k, y % bn], axis=k),
+                lambda x, y: tvm.sum(A[x, k].astype(dtype_output) * packedB[y / bn, k, y % bn].astype(dtype_output), axis=k),
                 name = 'C')
 
 s = tvm.create_schedule(C.op)
@@ -248,12 +322,13 @@ s[packedB].parallel(x)
 func = tvm.build(s, [A, B, C], target=target, name='mmult')
 assert func
 
-c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype), ctx)
+c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype_output), ctx)
 func(a, b, c)
 numpy.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
 
-evaluator = func.time_evaluator(func.entry_name, ctx, number=10)
-print('Opt4: %f' % evaluator(a, b, c).mean)
+evaluator = func.time_evaluator(func.entry_name, ctx, number=REPEAT)
+print('Opt4: %f' % (FLOPS / evaluator(a, b, c).mean / 1E9))
+
 
 ################################################################################################
 # Here is the generated IR after array packing.
@@ -294,12 +369,13 @@ s[packedB].parallel(x)
 func = tvm.build(s, [A, B, C], target=target, name='mmult')
 assert func
 
-c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype), ctx)
+c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype_output), ctx)
 func(a, b, c)
 numpy.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
 
-evaluator = func.time_evaluator(func.entry_name, ctx, number=10)
-print('Opt5: %f' % evaluator(a, b, c).mean)
+evaluator = func.time_evaluator(func.entry_name, ctx, number=REPEAT)
+print('Opt5: %f' % (FLOPS / evaluator(a, b, c).mean / 1E9))
+
 
 ################################################################################################
 # Here is the generated IR after blocking.
@@ -337,13 +413,13 @@ s[packedB].parallel(x)
 func = tvm.build(s, [A, B, C], target=target, name = 'mmult')
 assert func
 
-c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype), ctx)
+c = tvm.nd.array(numpy.zeros((M, N), dtype = dtype_output), ctx)
 func(a, b, c)
 numpy.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
 
-evaluator = func.time_evaluator(func.entry_name, ctx, number=50)
+evaluator = func.time_evaluator(func.entry_name, ctx, number=REPEAT)
 opt6_time = evaluator(a, b, c).mean
-print('Opt6: %f' % opt6_time)
+print('Opt6: %f' % (FLOPS / evaluator(a, b, c).mean / 1E9))
 
 ################################################################################################
 # Here is the generated IR after parallelization.
@@ -360,3 +436,71 @@ print(tvm.lower(s, [A, B, C], simple_mode=True))
 # Note that the outputs on the web page reflect the running times on a non-exclusive
 # Docker container, thereby they are *unreliable*. It is highly encouraged to run the
 # tutorial by yourself to observe the performance gain acheived by TVM.
+
+MTile = 8
+NTile = 8
+KTile = 4
+# microkernel
+
+# for m in M:
+#     for n in N:
+#         for kb in range(0, K, KB):
+#             for ko in range(KB):
+#                 c[m * N + n] += ((a[m * AStride + kb + ko] as int32)
+#                              * (b[kb * packedN + n * KB + ko] as int32))
+
+                # B[N * K] => BPacked[N / NB, K / KB, NB, KB]
+
+
+ATile = tvm.placeholder(
+    (M / MTile, K / KTile, MTile, KTile), dtype='uint8', name='ATile')
+BTile = tvm.placeholder(
+    (N / NTile, K / KTile, NTile, KTile), dtype='int8', name='BTile')
+kb = tvm.reduce_axis((0, K / KTile), name='kblock')
+kt = tvm.reduce_axis((0, KTile), name='ktile')
+C = tvm.compute(
+    (M / MTile, N / NTile, MTile, NTile),
+    lambda mb, nb, mt, nt: tvm.sum(
+        ATile[mb, kb, mt, kt].astype('int32') * BTile[nb, kb, nt, kt].astype('int32'),
+        axis=[kb, kt]),
+    name='C')
+
+C = tvm.compute(
+    (M, N),
+    lambda m, n: tvm.sum(
+        ATile[m // MTile, kb, m % MTile, kt].astype('int32') * BTile[n // NTile, kb, n % NTile, kt].astype('int32'),
+        axis=[kb, kt]),
+    name='C')
+
+print("C", C.shape)
+s = tvm.create_schedule(C.op)
+(m, n) = s[C].op.axis
+(mo, no, mi, ni) = s[C].tile(m, n, MTile, NTile)
+s[C].tensorize(mi, intrin_tile_gemm(KBlock=K / KTile, MTile=MTile, NTile=NTile, KTile=KTile))
+at = tvm.nd.array(numpy.random.randint(low=0, high=255, size=get_const_tuple(ATile.shape)).astype(ATile.dtype), ctx)
+bt = tvm.nd.array(numpy.random.randint(low=-63, high=63, size=get_const_tuple(BTile.shape)).astype(BTile.dtype), ctx)
+c = tvm.nd.array(numpy.zeros(get_const_tuple(C.shape), dtype=C.dtype), ctx)
+
+
+print("ATile.shape", ATile.shape)
+print("BTile.shape", BTile.shape)
+print("C.shape", C.shape)
+print(tvm.lower(s, [ATile, BTile, C], simple_mode=True))
+func = tvm.build(s, [ATile, BTile, C], target=target, name='tensorize')
+
+evaluator = func.time_evaluator(func.entry_name, ctx, number=REPEAT)
+opt6_time = evaluator(at, bt, c).mean
+print('OptTensorize: %f' % (FLOPS / evaluator(at, bt, c).mean / 1E9))
+print(c)
+
+def ref(at, bt):
+    result = numpy.zeros(c.shape, dtype=c.dtype)
+    for m in range(M):
+        for n in range(N):
+            acc = 0
+            for k in range(K):
+                acc += at[m // MTile, k // KTile, m % MTile, k % KTile] * bt[n // NTile, k // KTile, n % NTile, k % KTile]
+            result[m, n] = acc
+    return result
+
+numpy.testing.assert_allclose(c.asnumpy(), ref(at.asnumpy(), bt.asnumpy()))

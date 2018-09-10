3f30763154fa79d3ae81edf9ef0c5172263bccb9
Andrew Tulloch <andrew@tullo.ch>
2018-09-10 13:17:32 -0700

    TVM perf

--- unet/tvm_perf.py
+++ unet/tvm_perf.py
@@ -9,6 +9,7 @@ import tvm
 from tvm.contrib import graph_runtime as runtime
 import time
 import click
+import logging
 
 target = 'llvm -mcpu=core-avx2'
 ctx = tvm.context(str(target), 0)
@@ -19,8 +20,8 @@ ctx = tvm.context(str(target), 0)
 @click.option('--num_cycles', default=5)
 @click.option('--opt_level', default=3)
 def run(align_8, num_iter, num_cycles, opt_level):
-
-    sym = unet.unet_symbol(align_8=True)
+    logging.basicConfig(level=logging.DEBUG)
+    sym = unet.unet_symbol(align_8=align_8)
     mod = mx.mod.Module(symbol=sym, context=mx.cpu())
     mod.bind(for_training     = False,
              inputs_need_grad = False,
@@ -34,7 +35,7 @@ def run(align_8, num_iter, num_cycles, opt_level):
 
 
     module = runtime.create(graph, lib, ctx)
-
+    # logging.debug(graph.symbol().debug_str())
     module.set_input('data', tvm.nd.array(np.random.uniform(size=(data_shape)).astype("float32")))
     rparams = {k: tvm.nd.array(v.shape, ctx) for k, v in params.items()}
     module.set_input(**params)

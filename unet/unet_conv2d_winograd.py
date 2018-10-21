28df75b8cce51dd0d932878e1110cff42b9a1ec9
Andrew Tulloch <andrew@tullo.ch>
2018-10-20 22:48:50 -0700

    ASDF

---
+++ unet/unet_conv2d_winograd.py
@@ -0,0 +1,266 @@
+# pylint: disable=invalid-name,unused-variable,no-else-return
+from __future__ import absolute_import as _abs
+
+import numpy as np
+
+import tvm
+from tvm import autotvm
+
+from topi.generic import schedule_conv2d_nchw, schedule_conv2d_NCHWc_
+from topi.util import traverse_inline, get_const_tuple, const_matrix
+from topi.nn import pad, conv2d, conv2d_NCHWc, conv2d_alter_layout
+from topi.nn.util import get_const_int, get_pad_tuple
+import topi.nn
+
+import os
+UNROLL = int(os.environ.get('UNROLL', '1'))
+
+G_data = np.array([
+    [1,      0,     0],
+    [-2/9,  -2/9,   -2/9],
+    [-2/9,   2/9,   -2/9],
+    [1/90,  1/45,   2/45],
+    [1/90,  -1/45,  2/45],
+    [1/45,    1/90, 1/180],
+    [1/45,   -1/90, 1/180],
+    [0,      0,     1]
+], dtype=np.float32)
+
+B_data = np.array([
+    [1,   0,    -21/4,    0,    21/4,     0,    -1,  0],
+    [0,   1,      1,    -17/4,  -17/4,    1,    1,   0],
+    [0,   -1,     1,    17/4,   -17/4,   -1,    1,   0],
+    [0,  1/2,    1/4,   -5/2,   -5/4,     2,    1,   0],
+    [0,  -1/2,   1/4,    5/2,   -5/4,    -2,    1,   0],
+    [0,   2,      4,    -5/2,    -5,     1/2,   1,   0],
+    [0,   -2,     4,     5/2,    -5,    -1/2,   1,   0],
+    [0,   -1,     0,    21/4,     0,    -21/4,  0,   1]
+], dtype=np.float32).T
+
+A_data = np.array([
+    [1,  1,  1,   1,    1,    32,      32,    0],
+    [0,  1,  -1,  2,   -2,  16,   -16,   0],
+    [0,  1,  1,   4,    4,   8,    8,   0],
+    [0,  1,  -1,  8,   -8,   4,   -4,   0],
+    [0,  1,  1,   16,  16,   2,  2,   0],
+    [0,  1,  -1,  32,  -32,  1,  -1,  1]
+], dtype=np.float32).T
+
+
+def _decl_winograd_NCHWc(cfg, data, kernel, num_filter, kernel_size, stride, padding, layout, out_layout, out_dtype):
+    # import ipdb
+    # ipdb.set_trace()
+    # assert layout == "NCHW", "Only support NCHW"
+    # create workload according to raw arguments
+    # wkl = _conv_NCHWc_arg_to_workload(
+    #     data, kernel, num_filter, kernel_size,
+    #     stride, padding, layout, out_layout, out_dtype)
+    wkl = [] #None
+
+    out_dtype = out_dtype or data.dtype
+    N, CII, IH, IW, CIII = get_const_tuple(data.shape)
+    COO, CII, KH, KW, CIII_, VC = get_const_tuple(kernel.shape)
+
+    pad_top, pad_left, pad_bottom, pad_right = get_pad_tuple(padding, (KH, KW))
+    HSTR, WSTR = stride if isinstance(stride, (tuple, list)) else (stride, stride)
+
+    OH = (IH + pad_top + pad_bottom - KH) // HSTR + 1
+    OW = (IW + pad_left + pad_right - KW) // WSTR + 1
+    data_pad = pad(data, [0, 0, pad_top, pad_left, 0], [0, 0, pad_bottom, pad_right, 0], name="data_pad")
+
+    # ==================== define configuration space ====================
+    n, coo, oh, ow, vc = cfg.axis(N), cfg.axis(COO), cfg.axis(OH), cfg.axis(OW), cfg.axis(VC)
+    cii, ciii, kh, kw = cfg.reduce_axis(CII), cfg.reduce_axis(CIII), cfg.reduce_axis(KH), cfg.reduce_axis(KW)
+
+    oh, vh = cfg.define_split('tile_oh', oh, num_outputs=2, filter=lambda x: x.size[-1] <= 8)
+    ow, vw = cfg.define_split('tile_ow', ow, num_outputs=2, filter=lambda x: x.size[-1] <= 8)
+
+    cfg.define_reorder("reorder_0",
+                       [n, coo, cii, oh, ow, kh, kw, vc, ciii, vh, vw],
+                       policy='candidate', candidate=[
+                           [n, coo, cii, oh, ow, kh, kw, vc, ciii, vh, vw],
+                           [n, coo, cii, oh, ow, kh, kw, ciii, vh, vw, vc],
+                           [n, coo, cii, oh, ow, kh, kw, vc, vh, ciii, vw],
+                           [n, coo, cii, oh, ow, kh, kw, ciii, vh, vc, vw],
+                           [n, coo, oh, cii, ow, kh, kw, ciii, vh, vw, vc],
+                       ])
+
+    cfg.define_reorder("reorder_1",
+                       [n, coo, oh, ow, vh, vw, vc],
+                       policy='candidate', candidate=[
+                           [n, coo, oh, ow, vh, vw, vc],
+                           [n, coo, oh, ow, vc, vh, vw],
+                           [n, coo, oh, ow, vh, vc, vw]
+                       ])
+
+    cfg.define_annotate("ann_reduce", [kh, kw, ciii], policy='try_unroll')
+    cfg.define_annotate("ann_spatial", [vh, vw, vc], policy='try_unroll_vec')
+
+    VH = cfg["tile_oh"].size[-1]
+    VW = cfg["tile_ow"].size[-1]
+
+
+    dvshape = (N, CII, OH // VH, OW // VW, VH*HSTR + KH-1, VW*WSTR + KW-1, CIII)
+    ovshape = (N, COO, OH // VH, OW // VW, VH, VW, VC)
+    oshape = (N, COO, OH, OW, VC)
+
+    m = 6
+    r = 3
+    alpha = m + r - 1
+
+    # Layouts:
+
+    # input_tile_shape = (N, CII, OH // m, OH // m, alpha, alpha, CIII)
+    # U_shape = (COO, CII, alpha, alpha, CIII, COOO)
+    # V_shape = (N, CII, OH // m, OW // m, alpha, alpha, CIII)
+    # M_shape = (N, COO, OH // m, OW // m, alpha, alpha, COOO)
+    # Y_shape = (N, COO, OH // m, OW // m, m, m, COOO)
+    # O_shape = (N, COO, OH, OW, COOO)
+
+
+    def div_round_up(a, b):
+        return (a + b - 1) // b
+
+    input_tile = tvm.compute((N, CII, div_round_up(OH, m), div_round_up(OW, m), alpha, alpha, CIII),
+                             lambda n, cii, oh, ow, eps, nu, ciii:
+                             data_pad[n][cii][oh * m + eps][ow * m + nu][ciii],
+                             name='input_tile')
+
+
+    # transform kernel
+    G = const_matrix(G_data, 'G')
+    r_kh = tvm.reduce_axis((0, KH), 'r_kh')
+    r_kw = tvm.reduce_axis((0, KW), 'r_kw')
+    U = tvm.compute((COO, CII, alpha, alpha, CIII, VC),
+                    lambda coo, cii, eps, nu, ciii, vc:
+                    tvm.sum(kernel[coo][cii][r_kh][r_kw][ciii][vc].astype(out_dtype) *
+                            G[eps][r_kh] * G[nu][r_kw], axis=[r_kh, r_kw]),
+                    name='U')
+
+    # transform image
+    B = const_matrix(B_data, 'B')
+    r_eps = tvm.reduce_axis((0, alpha), 'r_eps')
+    r_nu = tvm.reduce_axis((0, alpha), 'r_nu')
+    V = tvm.compute((N, CII, div_round_up(OH, m), div_round_up(OW, m), alpha, alpha, CIII),
+                    lambda n, cii, oh, ow, eps, nu, ciii:
+                    tvm.sum(input_tile[n][cii][oh][ow][r_eps][r_nu][ciii].astype(out_dtype) *
+                            B[r_eps][eps] * B[r_nu][nu], axis=[r_eps, r_nu]), name='V')
+    cii = tvm.reduce_axis((0, CII), name='cii')
+    ciii = tvm.reduce_axis((0, CIII), name='ciii')
+
+    # M_shape = (N, COO, OH // m, OW // m, alpha, alpha, COOO)
+    M = tvm.compute((N, COO, div_round_up(OH, m), div_round_up(OW, m), alpha, alpha, VC),
+                    lambda n, coo, oh, ow, eps, nu, vc:
+                    tvm.sum(U[coo][cii][eps][nu][ciii][vc] * V[n][cii][oh][ow][eps][nu][ciii],
+                            axis=[cii, ciii]),
+                    name='M')
+
+    # inverse transform
+    A = const_matrix(A_data, 'A')
+    r_eps = tvm.reduce_axis((0, alpha), 'r_eps')
+    r_nu = tvm.reduce_axis((0, alpha), 'r_nu')
+    # Y_shape = (N, COO, OH // m, OW // m, m, m, COOO)
+    Y = tvm.compute((N, COO, div_round_up(OH, m), div_round_up(OW, m), m, m, VC),
+                    lambda n, coo, oh, ow, vh, vw, vc:
+                    tvm.sum(M[n][coo][oh][ow][r_eps][r_nu][vc] * A[r_eps][vh] * A[r_nu][vw],
+                            axis=[r_eps, r_nu]),
+                    name='Y')
+
+    output = tvm.compute((N, COO, OH, OW, VC),
+                         lambda n, coo, oh, ow, vc:
+                         Y[n][coo][oh // m][ow // m][oh % m][ow % m][vc],
+                         name='output', tag='winograd_conv2d_output',
+                         attrs={'workload': wkl})
+
+    cfg.add_flop(2 * N * COO * OH * OW * KH * KW * CII * CIII)
+    return output
+
+def _schedule_winograd_NCHWc(cfg, s, output, last):
+    Y = output.op.input_tensors[0]
+    M, A = Y.op.input_tensors
+    U, V = M.op.input_tensors
+    d, B = V.op.input_tensors
+    data_pad = d.op.input_tensors[0]
+
+    # padding
+    s[data_pad].compute_inline()
+
+    # pack input tiles
+    s[d].compute_inline()
+
+    # transform kernel
+    if isinstance(U.op, tvm.tensor.ComputeOp):
+        kernel, G = U.op.input_tensors
+        s[G].compute_inline()
+        coo, cii, eps, nu, ciii, vc = s[U].op.axis
+        if autotvm.GLOBAL_SCOPE.in_tuning:
+            # kernel transformation will be pre-computed during compilation, so we skip
+            # this part to make tuning records correct
+            s[U].pragma(eps, 'debug_skip_region')
+        else:
+            pass
+            # r_kh, r_kw = s[U].op.reduce_axis
+            # s[U].reorder(k, c, eps, nu, r_kh, r_kw, kk)
+            # for axis in [eps, nu, r_kh, r_kw]:
+            #     s[U].unroll(axis)
+            # s[U].vectorize(kk)
+            # s[U].parallel(k)
+
+        if isinstance(kernel.op, tvm.tensor.ComputeOp) and "dilate" in kernel.op.tag:
+            s[kernel].compute_inline()
+
+    # transform image
+    DD = s.cache_read(d, 'global', [V])
+    s[B].compute_inline()
+    n, cii, oh, ow, eps, nu, ciii = s[V].op.axis
+    r_eps, r_nu = s[V].op.reduce_axis
+    # s[V].reorder(b, c, eps, nu, r_eps, r_nu, bb)
+    for axis in [eps, nu, r_eps, r_nu]:
+        if UNROLL:
+            s[V].unroll(axis)
+    s[DD].compute_at(s[V], cii)
+    s[V].vectorize(ciii)
+    # s[V].parallel(b)
+
+    # batch gemm
+    n, coo, oh, ow, eps, nu, vc = s[M].op.axis
+    cii, ciii = s[M].op.reduce_axis
+    s[M].vectorize(vc)
+    # cfg.define_split('tile_c', c, num_outputs=2, filter=lambda x: x.size[-1] <= 16)
+    # co, ci = cfg['tile_c'].apply(s, M, c)
+    # xo, xi = cfg['tile_p'].apply(s, M, b)
+    # s[M].reorder(eps, nu, xo, co, k, ci, xi)
+    # cfg.define_annotate('ann_reduce', [ci], policy='try_unroll')
+    # cfg.define_annotate('ann_spatial', [k, xi], policy='try_unroll_vec')
+    # cfg['ann_reduce'].apply(s, M, [ci],
+    #                         axis_lens=[cfg['tile_c'].size[-1]],
+    #                         max_unroll=16,
+    #                         cfg=cfg)
+    # cfg['ann_spatial'].apply(s, M, [k, xi])
+
+    # inverse transform
+    s[A].compute_inline()
+    n, coo, oh, ow, vh, vw, vc = s[Y].op.axis
+    r_eps, r_nu = s[Y].op.reduce_axis
+    for axis in [vh, vw, r_eps, r_nu]:
+        if UNROLL:
+            s[Y].unroll(axis)
+    s[Y].vectorize(vc)
+    # output
+
+    n, coo, h, w, vc = s[last].op.axis
+    s[last].vectorize(vc)
+    s[output].vectorize(vc)
+    # co, coi = cfg['tile_k'].apply(s, last, co)
+    # s[M].compute_at(s[last], co)
+    # s[last].parallel(co)
+
+    # MM = s.cache_read(M, 'global', [Y])
+    # m = get_const_int(V.shape[0]) + 1 - 3
+    # ho, wo, hi, wi = s[last].tile(h, w, m, m)
+    # s[Y].compute_at(s[last], wo)
+    # s[MM].compute_at(s[last], wo)
+
+    # if output != last:
+    #     s[output].compute_inline()
+    return s
--- unet/unet_winograd_benchmark.py
+++ unet/unet_winograd_benchmark.py
@@ -1,47 +1,69 @@
 from __future__ import division
 
+import click
 from topi.util import get_const_int, const_matrix
+from topi.nn.conv2d import Workload
 import numpy as np
 import tvm
 import tvm.rpc
 from tvm import autotvm
-import unet_winograd_NCHWc
 import collections
 import logging
 import sys
-
+import unet_conv2d_winograd
 
 @autotvm.template
-def conv2d_NCHWc_winograd_autotvm(s, ic, oc):
-    ic = ((ic + 7) // 16) * 16
-    oc = ((oc + 7) // 16) * 16
+def conv2d_NCHWc_winograd_autotvm(s, ic, oc, kernel, pad, stride):
+    # ic = ((ic + 16 - 1) // 16) * 16
+    # oc = ((oc + 16 - 1) // 16) * 16
     cfg = autotvm.get_config()
-    cfg.define_knob('unroll', [1])
-    cfg.define_knob('compute_at', [0])
-    cfg.define_knob('vectorize', [1])
-    cfg.define_knob('tensorize', [1])
-    cfg.define_knob('VK,VP', [(4, 24), (6, 16), (5, 16)])
+    cfg.define_knob('BNInput', [4, 8, 16]) # TODO, 8, 16
+    cfg.define_knob('BNOutput', [8, 16]) # TODO 8, 16
+    BNInput = cfg['BNInput'].val
+    BNOutput = cfg['BNOutput'].val
+    X = tvm.placeholder(shape=(1, ic // BNInput, s, s, BNInput), dtype="float32", name="X")
+    W = tvm.placeholder(shape=(oc // BNOutput, ic // BNInput, kernel, kernel, BNInput, BNOutput), dtype="float32", name="W")
 
-    for intermediate in ["M", "A_T_dot_M", "input_tile", "B_T_dot_X", "V", "Y"]:
-        cfg.define_knob("{}_COMPUTE_AT".format(intermediate), [1])
-    for intermediate in ["input_tile", "V", "B_T_dot_X"]: # , "B_T_dot_X",
-        cfg.define_knob("{}_REORDER_C".format(intermediate), [0, 1])
-
-    cfg.define_knob('data_pad_inline', [1])
-    (VK, VP) = cfg['VK,VP'].val
-    X = tvm.placeholder(shape=(1, ic // 8, s, s, 8), dtype="float32", name="X")
-    W = tvm.placeholder(shape=(oc // 8, ic // 8, 3, 3, 8, 8), dtype="float32", name="W")
-
-    output = unet_winograd_NCHWc.decl_winograd_NCHWc(cfg, X, W, strides=1, padding=1, out_dtype="float32", VK=VK, VP=VP)
-    s = unet_winograd_NCHWc.schedule_winograd_NCHWc(cfg, output, VK=VK, VP=VP)
-    if cfg.flop == 0:
-        cfg.add_flop(2 * ic * oc * s * s * 3 * 3)
-    return s, [X, W, output]
+    Y = unet_conv2d_winograd._decl_winograd_NCHWc(cfg, X, W, num_filter=oc, kernel_size=kernel, stride=stride, padding=pad, layout="NCHW{}c".format(BNInput), out_layout="NCHW{}c".format(BNOutput), out_dtype="float32")
+    s = tvm.create_schedule([Y.op])
+    s = unet_conv2d_winograd._schedule_winograd_NCHWc(cfg, s, Y, Y)
+    print(tvm.lower(s, [X, W, Y], simple_mode=True))
+    return s, [X, W, Y]
 
 
-Workload = collections.namedtuple("Workload", ["space", "input_channel", "output_channel", "kernel", "pad", "stride"])
+# Workload = collections.namedtuple("Workload", ["space", "input_channel", "output_channel", "kernel", "pad", "stride"])
 
 WORKLOADS = [
+        # Workload('float32', 'float32', 224, 224, 3, 64, 7, 7, 3, 3, 2, 2),
+        # Workload('float32', 'float32', 56, 56, 64, 64, 3, 3, 0, 0, 1, 1),
+        # Workload('float32', 'float32', 56, 56, 64, 64, 3, 3, 1, 1, 1, 1),
+        Workload('float32', 'float32', 56, 56, 64, 64, 1, 1, 0, 0, 1, 1),
+        Workload('float32', 'float32', 56, 56, 64, 128, 3, 3, 1, 1, 2, 2),
+        Workload('float32', 'float32', 56, 56, 64, 128, 1, 1, 0, 0, 2, 2),
+        Workload('float32', 'float32', 28, 28, 128, 128, 3, 3, 1, 1, 1, 1),
+        Workload('float32', 'float32', 28, 28, 128, 256, 3, 3, 1, 1, 2, 2),
+        Workload('float32', 'float32', 28, 28, 128, 256, 1, 1, 0, 0, 2, 2),
+        Workload('float32', 'float32', 14, 14, 256, 256, 3, 3, 1, 1, 1, 1),
+        Workload('float32', 'float32', 14, 14, 256, 512, 3, 3, 1, 1, 2, 2),
+        Workload('float32', 'float32', 14, 14, 256, 512, 1, 1, 0, 0, 2, 2),
+        Workload('float32', 'float32', 7, 7, 512, 512, 3, 3, 1, 1, 1, 1),
+        # workloads of resnet34_v1 on imagenet, no extra workload required
+        # workloads of resnet50_v1 on imagenet
+        Workload('float32', 'float32', 56, 56, 64, 256, 1, 1, 0, 0, 1, 1),
+        Workload('float32', 'float32', 56, 56, 256, 64, 1, 1, 0, 0, 1, 1),
+        Workload('float32', 'float32', 56, 56, 256, 128, 1, 1, 0, 0, 2, 2),
+        Workload('float32', 'float32', 28, 28, 128, 512, 1, 1, 0, 0, 1, 1),
+        Workload('float32', 'float32', 56, 56, 256, 512, 1, 1, 0, 0, 2, 2),
+        Workload('float32', 'float32', 28, 28, 512, 128, 1, 1, 0, 0, 1, 1),
+        Workload('float32', 'float32', 28, 28, 512, 256, 1, 1, 0, 0, 2, 2),
+        Workload('float32', 'float32', 14, 14, 256, 1024, 1, 1, 0, 0, 1, 1),
+        Workload('float32', 'float32', 28, 28, 512, 1024, 1, 1, 0, 0, 2, 2),
+        Workload('float32', 'float32', 14, 14, 1024, 256, 1, 1, 0, 0, 1, 1),
+        Workload('float32', 'float32', 14, 14, 1024, 512, 1, 1, 0, 0, 2, 2),
+        Workload('float32', 'float32', 7, 7, 512, 2048, 1, 1, 0, 0, 1, 1),
+        Workload('float32', 'float32', 14, 14, 1024, 2048, 1, 1, 0, 0, 2, 2),
+        Workload('float32', 'float32', 7, 7, 2048, 512, 1, 1, 0, 0, 1, 1),
+
         # Workload(space=102, input_channel=128, output_channel=128, kernel=3, pad=1, stride=1),
         # # Workload(space=102, input_channel=32, output_channel=32, kernel=3, pad=1, stride=1),
         # # Workload(space=56, input_channel=64, output_channel=64, kernel=3, pad=1, stride=1),
@@ -54,48 +76,70 @@ WORKLOADS = [
 
         # # # Workload(space=12, input_channel=256, output_channel=256, kernel=3, pad=1, stride=1),
         # Workload(space=192, input_channel=3, output_channel=12, kernel=3, pad=1, stride=1),
-        Workload(space=96, input_channel=12, output_channel=24, kernel=3, pad=1, stride=1),
-        Workload(space=48, input_channel=24, output_channel=48, kernel=3, pad=1, stride=1),
-        Workload(space=24, input_channel=48, output_channel=96, kernel=3, pad=1, stride=1),
-        Workload(space=12, input_channel=96, output_channel=180, kernel=3, pad=1, stride=1),
-        Workload(space=6, input_channel=180, output_channel=220, kernel=3, pad=1, stride=1),
-        Workload(space=6, input_channel=220, output_channel=180, kernel=3, pad=1, stride=1),
-        Workload(space=12, input_channel=180, output_channel=96, kernel=3, pad=1, stride=1),
-        Workload(space=24, input_channel=96, output_channel=48, kernel=3, pad=1, stride=1),
-        Workload(space=48, input_channel=48, output_channel=24, kernel=3, pad=1, stride=1),
-        Workload(space=96, input_channel=24, output_channel=12, kernel=3, pad=1, stride=1),
+        # Workload(space=96, input_channel=12, output_channel=24, kernel=3, pad=1, stride=1),
+        # Workload(space=48, input_channel=24, output_channel=48, kernel=3, pad=1, stride=1),
+        # Workload(space=24, input_channel=48, output_channel=96, kernel=3, pad=1, stride=1),
+        # Workload(space=12, input_channel=96, output_channel=180, kernel=3, pad=1, stride=1),
+        # Workload(space=6, input_channel=180, output_channel=220, kernel=3, pad=1, stride=1),
+        # Workload(space=6, input_channel=220, output_channel=180, kernel=3, pad=1, stride=1),
+        # Workload(space=12, input_channel=180, output_channel=96, kernel=3, pad=1, stride=1),
+        # Workload(space=24, input_channel=96, output_channel=48, kernel=3, pad=1, stride=1),
+        # Workload(space=48, input_channel=48, output_channel=24, kernel=3, pad=1, stride=1),
+        # Workload(space=96, input_channel=24, output_channel=12, kernel=3, pad=1, stride=1),
         # Workload(space=192, input_channel=12, output_channel=1, kernel=3, pad=1, stride=1),
 ]
 
-def run():
+target = 'llvm -mcpu=skylake-avx512 -target=x86_64-linux-gnu'
+local_target = 'llvm -mcpu=core-avx2'
+
+@click.command()
+@click.option('--autotvm_number', default=50)
+@click.option('--autotvm_repeat', default=4)
+@click.option('--autotvm_n_trial', default=200)
+@click.option('--autotvm_early_stopping', default=100)
+@click.option('--autotvm_log', default="autotvm_winograd_benchmark.log", type=str)
+@click.option('--layout', type=click.Choice(["NCHWc"]), required=True)
+@click.option('--tracker_port', default=9195)
+@click.option('--local', is_flag=True, default=False)
+def run(layout,
+        autotvm_number,
+        autotvm_repeat,
+        autotvm_log,
+        autotvm_n_trial,
+        autotvm_early_stopping,
+        tracker_port,
+        local):
     logging.basicConfig(level=logging.DEBUG)
     for i, w in enumerate(WORKLOADS):
+        if w.in_filter % 16 != 0 or w.out_filter % 16 != 0:
+            continue
         measure_option=autotvm.measure_option(
-            builder=autotvm.LocalBuilder(n_parallel=1, timeout=500),
-            runner=autotvm.LocalRunner(
-                number=50,
-                repeat=4,
-                timeout=500)
+            builder=autotvm.LocalBuilder(timeout=10),
+            runner=autotvm.RPCRunner(
+                'skl', 'localhost', tracker_port,
+                number=autotvm_number,
+                repeat=autotvm_repeat,
+                timeout=10) if not local else
+            autotvm.LocalRunner(
+                timeout=10,
+                number=autotvm_number,
+                repeat=autotvm_repeat)
         )
 
         task = autotvm.task.create(
             conv2d_NCHWc_winograd_autotvm,
-            args=(w.space, w.input_channel, w.output_channel),
-            target=tvm.target.create('llvm -mcpu=core-avx2'))
+            args=(w.height, w.in_filter, w.out_filter, w.hkernel, w.hpad, w.hstride),
+            target=tvm.target.create(target if not local else local_target))
         print(task.config_space)
         tuner = autotvm.tuner.XGBTuner(task, feature_type="knob")
-        job_name = 'conv2d_minimal_winograd_{w.space}_{w.input_channel}_{w.output_channel}'.format(w=w)
-        # try:
-        #     tuner.load_history(autotvm.record.load_from_file("{}.log".format(job_name)))
-        # except Exception as e:
-        #     logging.exception("Failed to load history file")
-        n_trial = 200
         tuner.tune(
-            n_trial=n_trial,
+            n_trial=autotvm_n_trial,
             measure_option=measure_option,
             callbacks=[
-                autotvm.callback.progress_bar(n_trial, prefix=job_name),
-                autotvm.callback.log_to_file('{}.log'.format(job_name))])
+                autotvm.callback.progress_bar(
+                    autotvm_n_trial,
+                    prefix="{w.height}S, {w.in_filter} -> {w.out_filter}, {w.hkernel}K, {w.hpad}P, {w.hstride}s, {layout}".format(w=w, layout=layout)),
+                autotvm.callback.log_to_file(str(autotvm_log))])
 
 if __name__ == "__main__":
     run()

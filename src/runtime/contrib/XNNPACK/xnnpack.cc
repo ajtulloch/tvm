cbd44158e4669e6dc72d1ba33239138f64d78717
Andrew Tulloch <andrew@tullo.ch>
2020-03-19 16:00:07 -0700

    asdf

--- src/runtime/contrib/XNNPACK/xnnpack.cc
+++ src/runtime/contrib/XNNPACK/xnnpack.cc
@@ -15,12 +15,6 @@
 namespace std {
 namespace {
 
-// Code from boost
-// Reciprocal of the golden ratio helps spread entropy
-//     and handles duplicates.
-// See Mike Seymour in magic-numbers-in-boosthash-combine:
-//     https://stackoverflow.com/questions/4948780
-
 template <class T>
 inline void hash_combine(std::size_t& seed, T const& v) {
   seed ^= hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
